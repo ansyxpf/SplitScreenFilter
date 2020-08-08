f6e9ba46bb30b9293254235f71e5b589517c4547
XuPengfei <xupengfei@thtf.com.cn>
2020-08-08 15:54:51 +0800

    提交项目

---
+++ SplitScreenFilter/SplitScreenFilter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SplitScreenFilter
+//
+//  Created by 徐鹏飞 on 2020/8/8.
+//  Copyright © 2020 徐鹏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
